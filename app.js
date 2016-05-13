ab500ebf6e9d7c8a873595d500743a451e5e8ff1
lzbin <1005624043@qq.com>
2016-05-13 17:39:39 +0800

    add spa file

--- app.js
+++ app.js
@@ -12,43 +12,55 @@
 
 // ------------ BEGIN MODULE SCOPE VARIABLES --------------
 'use strict';
-var
-    http    = require( 'http'     ),
+var http = require( 'http'),
     bodyParser = require('body-parser'),
-    express = require( 'express'  ),
-    routes  = require( './routes' ),
+    methodOverride = require('method-override'),
+    logger = require('morgan'),
+    errorhandler = require('errorhandler'),
+    express = require( 'express'),
+    auth = require('basic-auth'),
+    io = require('socket.io'),
+    routes = require( './routes'),
 
-    app     = express(),
-    server  = http.createServer( app );
+    app = express(),
+    server = http.createServer( app );
 // ------------- END MODULE SCOPE VARIABLES ---------------
-
 // ------------- BEGIN SERVER CONFIGURATION ---------------
+var env = app.get('env');
 
-app.use( express.bodyParser() );
-app.use( express.methodOverride() );
-app.use( express.basicAuth( 'user', 'spa' ) );
-app.use( express.static( __dirname + '/public' ) );
-app.use( app.router );
+app.use(bodyParser());
+app.use(methodOverride());
 
-if( 'development' == app.get('env')) {
-    app.use( express.logger() );
-    app.use( express.errorHandler({
-            dumpExceptions : true,
-            showStack      : true
-        })
-    );
+app.use(function(req, res, next) {
+    var user = auth(req);
+
+    if (user === undefined || user['name'] !== 'spa' || user['pass'] !== 'spa') {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+        res.end('Unauthorized');
+    } else {
+        next();
+    }
+});
+
+app.use(express.static(__dirname + '/public'));
+//app.use(app.router);
+
+if('development' == env){
+    //app.use(logger());
+    app.use(errorhandler({
+            dumpExceptions: true,
+            showStrack : true
+        }
+    ));
 }
-if( 'production' == app.get('env')){
-    app.use( express.errorHandler() );
+
+if('production' == env){
+    app.use(errorhandler());
 }
-
-routes.configRoutes( app, server );
-// -------------- END SERVER CONFIGURATION ----------------
-
-// ----------------- BEGIN START SERVER -------------------
-server.listen( 3000 );
-console.log(
-    'Express server listening on port %d in %s mode',
-    server.address().port, app.settings.env
-);
-// ------------------ END START SERVER --------------------
+routes.configRoutes(app,server);
+//---------------END SERVER CONFIGURATION------------------
+//---------------BEGIN START SERVER----------------------
+server.listen(3000);
+console.log('Express server listening on port %d in %s mode',server.address().port,app.settings.env);
+//----------------END START SERVER-------------------
